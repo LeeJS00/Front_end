ec5d909beb7c686603396b224d82ac64aa781719
LeeJS00 <jae2seung@naver.com>
2020-09-11 13:07:10 +0900

    last

--- front_end/src/components/Prob2.js
+++ front_end/src/components/Prob2.js
@@ -64,7 +64,7 @@ export default class Prob2 extends Component {
           <form>
             <input
               className="answer"
-              placeholder="정답 형식: 숫자"
+              placeholder="정답 형식: 영어"
               type="text"
               required
               value={this.state.answer}
