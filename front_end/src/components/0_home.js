038ce6d125c6c2e4f13e4d6710837b399259d482
LeeJS00 <jae2seung@naver.com>
2020-09-14 14:38:23 +0900

    front

--- front_end/src/components/0_home.js
+++ front_end/src/components/0_home.js
@@ -15,6 +15,7 @@ export default class Home extends Component {
       <div>
         <div className="story">
             <div className="marginbottom">오탈자 및 오류 발견시 <text style={{fontSize: "large"}}><p></p>01047843914,<p></p>andrew1208@postech.ac.kr<p></p>홍보팀장 박재완<p></p></text>으로 연락주세요.
+            <p></p><br></br><text style={{fontSize: "large", fontWeight:"bold", color:"red"}}>미궁 답은 대소문자, 띄어쓰기를 구분합니다. </text>
             </div>
           </div>
         <div className="center">
